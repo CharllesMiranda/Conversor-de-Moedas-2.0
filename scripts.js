dc8eeaa0c10b8c9310bae7af89aa56606fc11382
CharllesMiranda <devcharllesmiranda@gmail.com>
2026-06-19 19:51:18 -0300

    formatando moedas do real e dolar

--- scripts.js
+++ scripts.js
@@ -2,12 +2,23 @@ const convertButton = document.querySelector(".myButton")
 
 function convertValues() {
     const valueToConvert = document.querySelector("#valueToConvert").value
+    const valueForConversion = document.querySelector(".valueOne")
+    const ConvertedValue = document.querySelector(".valueTwo")
 
     const dolarToday = 5.20
 
     const convertedValue = valueToConvert / dolarToday
+
+    valueForConversion.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(valueToConvert)
+
+    ConvertedValue.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
     
-    console.log(convertedValue)
 }
 
 convertButton.addEventListener("click", convertValues)
